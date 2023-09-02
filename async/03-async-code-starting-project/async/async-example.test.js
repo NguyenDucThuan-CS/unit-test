5b96c72c3226ebd00036cec2250460645dd73cbf
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-09-02 23:33:48 +0700

    test promise

--- async/03-async-code-starting-project/async/async-example.test.js
+++ async/03-async-code-starting-project/async/async-example.test.js
@@ -1,11 +1,23 @@
-import { it } from "vitest"
+import { it, expect } from "vitest"
 import { generateToken } from "./async-example"
-import { expect } from "chai"
+import { generateTokenPromise } from "./async-example"
 
 
-it('should generate token value', () => {
-    const testUserEmail = 'test@test.com'
-    generateToken(testUserEmail, (err, token) => {
-        expect(token).toBeDefined()
-    })
+it('should generate token value', (done) => {
+        const testUserEmail = 'test@test.com'
+        generateToken(testUserEmail, (err, token) => {
+            try {
+                expect(token).toBeDefined();
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        
+        })
+})
+
+it("should generate token value", () => {
+    const testUserEmail = 'test@test.com';
+    expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined()
 })
